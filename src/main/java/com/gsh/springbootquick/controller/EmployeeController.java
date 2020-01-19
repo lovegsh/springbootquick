ec5c26d94b1336b3ff44b1b9ed54b8d07fdf4f18
GanSiH <592242859@qq.com>
2020-01-19 17:32:30 +0800

    40

--- src/main/java/com/gsh/springbootquick/controller/EmployeeController.java
+++ src/main/java/com/gsh/springbootquick/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -40,5 +41,13 @@ public class EmployeeController {
         Collection<Department> departments = departmentDao.getDepartments();
         model.addAttribute("depts", departments);
         return "emp/add";
+    }
+
+    //员工添加
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        System.out.println("保存的员工信息： "+employee);
+        //来到员工列表页面   redirect: 重定向   forward: 转发
+        return "redirect:/emps";
     }
 }
